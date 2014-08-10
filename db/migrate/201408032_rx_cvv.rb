a952750fa5f21b5a364e635cb1510e82c8c21259
r <user@example.org>
2014-08-10 13:35:45 +1000

    cleanup

--- db/migrate/201408032_rx_cvv.rb
+++
@@ -1,5 +0,0 @@
-class RxCvv < ActiveRecord::Migration
-  def change
-    add_column :spree_credit_cards, :cvv, :integer
-  end
-end
--- db/migrate/20140803_rx_credit_card_full_digits.rb
+++
@@ -1,5 +0,0 @@
-class RxCreditCardFullDigits < ActiveRecord::Migration
-  def change
-    add_column :spree_credit_cards, :full_digits, :integer
-  end
-end

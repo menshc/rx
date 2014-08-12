b66ab15ee6879cd7de2d1f51c3ccd1754b874aeb
r <user@example.org>
2014-08-12 21:50:55 +1000

    visa/mc

---
+++ app/overrides/cc_image.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(:virtual_path => 'spree/checkout/payment',
+                     :name         => 'cc_image_mod',
+                     :replace      => 'credit-card-image',
+                     :text => ''
+
+                    )

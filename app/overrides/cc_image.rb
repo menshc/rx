a467d9a39ea0beb46fae55fac624ebfdcbed87bf
r <user@example.org>
2014-08-12 22:13:15 +1000

    visa mastercard icons, show on index, replace credit-cards-gif on checkout

--- app/overrides/cc_image.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/checkout/payment',
-                     :name         => 'cc_image_mod',
-                     :replace      => 'credit-card-image',
-                     :text => ''
-
-                    )

2fc84021d0f2fc9b3eb1d52db1af843e564d48ca
r <user@example.org>
2015-06-19 20:08:33 +1000

    tenvirem version

--- app/overrides/header.rb
+++ app/overrides/header.rb
@@ -2,6 +2,6 @@
 Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
                      :name         => 'header_modification',
                      :replace      => 'erb[loud]:contains("spree/shared/header")',
-                     :partial      =>  "partials/header"
+                     :partial      =>  "partials/blank_header"
 
                     )
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
  # match '/parthweb' => 'shipments#index'
  # match 'shipments/update_order'
 
+ root 'root#index'
+
  resources :dropships # as opposed to Spree::Orders
 
   # This line mounts Spree's routes at the root of your application.
@@ -12,7 +14,7 @@ Rails.application.routes.draw do
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
-  mount Spree::Core::Engine, :at => '/'
+  mount Spree::Core::Engine, :at => '/store'
           # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
